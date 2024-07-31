f50a66a7e99bebbc83e4a1b0b0b69913677ce3d5
Donquellso <kamil080801@gmail.com>
2024-07-31 20:21:26 +0200

    Small DOM update

--- dom.js
+++ dom.js
@@ -5,3 +5,18 @@ const js = jsstuff();
 js.addNewProject('Project 1');
 js.getAllProjectsNames();
 
+const DOM = () => {
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+
+
+
+
+
+
+
+
+
+
+
+}
